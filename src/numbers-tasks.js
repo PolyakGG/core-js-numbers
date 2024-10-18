6712e0a64d57870438679fbf8b3d7e8034346f8d
PolyakGG <separik8@gmail.com>
2024-10-18 23:49:19 +0400

    resolve some simply tasks(before line 100)

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -18,8 +18,8 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
 
 /**
@@ -33,8 +33,8 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * Math.PI * radius;
 }
 
 /**
@@ -49,10 +49,20 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+// eto jopa что было до этого
+// function getAverage(value1, value2) {
+//   if (value1 > Number.MAX_VALUE || value2 > Number.MAX_VALUE) {
+//     return Number.MAX_VALUE;
+//   }
+//   if (value1 >= 0 && value2 >= 0) {
+//     return Math.round((value1 + value2) / 2);
+//   } else {
+//     return null;
+//   }
+// }
+function getAverage(value1, value2) {
+  return value1 / 2 + value2 / 2;
 }
-
 /**
  * Returns a distance between two points by cartesian coordinates.
  *
@@ -68,8 +78,13 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+/** Изначально пробовал в
+ *  return Math.sqrt(x1 ** 2 - y1 ** 2 + (x2 ** 2 - y2 ** 2));
+ */
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const xxx = x2 - x1;
+  const yyy = y2 - y1;
+  return Math.sqrt(xxx * xxx + yyy * yyy);
 }
 
 /**
